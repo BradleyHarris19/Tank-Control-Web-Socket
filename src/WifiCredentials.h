b543c752850d040ca79120c30c36551b8722f5fa
Bradley Harris <47424953+BradleyHarris19@users.noreply.github.com>
2019-02-07 22:42:14 +0000

    Update WifiCredentials.h

--- src/WifiCredentials.h
+++ src/WifiCredentials.h
@@ -1,3 +1,3 @@
 // Credntials for connecting to Wifi
-static const char wifiSSID[] = "Jedha";
-static const char wifiPassword[] = "rogue-one";
+static const char wifiSSID[] = "(WiFi name)";
+static const char wifiPassword[] = "(password)";
